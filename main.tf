c9368b6e6018e60ceda29869ba1725bd3acd3f0a
ChouyassFortee <yassine.choujaa@forteeuniforme.onmicrosoft.com>
2026-03-18 01:52:43 -0400

    ajout de la vm linux 6

--- main.tf
+++ main.tf
@@ -26,21 +26,21 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-cr460-yassine"
+  name                = "vnet-cr460-yassine-v2"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
+  name                 = "subnet1-v2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet2"
+  name                 = "subnet2-v2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
