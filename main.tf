8cf99fad2acd2524a031b9d0064bd623dfed86a0
ChouyassFortee <yassine.choujaa@forteeuniforme.onmicrosoft.com>
2026-03-17 23:02:09 -0400

    added ressource group

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
-#main
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  cloud {
+    organization = "TP-CR460-YassineChoujaa"
+    workspaces {
+      name = "CR460-TP"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-cr460-yassine"
+  location = "canadacentral"
+  tags = {
+    environment = "TPcr460"
+  }
+}
