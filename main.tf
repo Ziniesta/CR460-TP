9f49266c5d056fbec9f82e836679bc2f9cb3fc1f
ChouyassFortee <yassine.choujaa@forteeuniforme.onmicrosoft.com>
2026-03-18 01:59:10 -0400

    change size vm

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                  = "vm-cr460-yassine"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B1s"
+  size                  = "Standard_D2_v4"
   admin_username        = "yassineAdmin"
   network_interface_ids = [azurerm_network_interface.vm_nic.id]
 
