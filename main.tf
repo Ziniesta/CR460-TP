9cdbf6f034b0eea732c5ce611103dbe27e72fd8a
ChouyassFortee <yassine.choujaa@forteeuniforme.onmicrosoft.com>
2026-03-18 00:06:03 -0400

    ajout virtual network

--- main.tf
+++ main.tf
@@ -25,4 +25,24 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+module "avm-res-network-virtualnetwork" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "0.8.1"
 
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location 
+
+  name         = "vnet-cr460-yassine"
+  address_space = ["10.0.0.0/16"]
+
+  subnets = {
+    subnet1 = {
+      name             = "subnet1"
+      address_prefixes = ["10.0.0.0/24"]
+    }
+    subnet2 = {
+      name             = "subnet2" 
+      address_prefixes = ["10.0.1.0/24"]
+    }
+  }
+}
